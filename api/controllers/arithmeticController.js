50e00a1deb3dd95d6ff92a95fd0f88ebd6a018a3
Laks1439 <104177308+Laks1439@users.noreply.github.com>
2022-04-21 18:29:36 -0700

    Update arithmeticController.js
    fix syntax

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,7 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    . Use + operator to typecast
+    // Use + operator to typecast
     'add': function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
